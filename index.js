3605f69d7e927b2935c9b3ee95d888e5722cbe38
Nandi Wong <nandi0315@gmail.com>
2019-04-04 11:46:36 +0800

    add run command

---
+++ index.js
@@ -0,0 +1,108 @@
+const Promise = require('bluebird');
+const restify = require('restify');
+const request = Promise.promisifyAll(require('request'));
+const addressParser = require('hk-address-parser-lib');
+
+
+async function searchAddress(req, res, next) {
+  const { address, lang } = req.query;
+  const addresses = await addressParser.parse(address);
+
+  const language = lang && lang.toLowerCase() === 'zh_hk' ? addressParser.Address.LANG_ZH : addressParser.Address.LANG_EN;
+  const results = addresses.map(address => {
+    const components = {};
+    const coordinate = address.coordinate();
+    address.components(language).forEach(function (component) {
+      components[component.key] = component.translatedValue;
+    });
+
+    return {
+      full_address: address.fullAddress(language),
+      coordinate: {
+        lat: parseFloat(coordinate.lat),
+        lng: parseFloat(coordinate.lng),
+      },
+      data_source: address.dataSource(),
+      elements: components,
+    }
+  });
+
+
+  res.send({
+    success: true,
+    data: {
+      search_string: address,
+      results,
+    }
+  });
+  next();
+}
+
+const asyncMiddleware = (fn) => {
+  return (req, res, next) => {
+    return fn(req, res, next).catch(error => {
+      console.error(error);
+      next(error);
+    })
+  }
+}
+
+const server = restify.createServer();
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+// Catch the errors and format the response
+server.on('restifyError', (req, res, err, callback) => {
+  logger.debug(err.message);
+  logger.debug(err.stack);
+  err.toJSON = function customToJSON() {
+    return {
+      success: false,
+      error_code: err.code || ERROR_SERVER_EXCEPTION,
+      error_message: err.message,
+    };
+  };
+  err.toString = function customToString() {
+    return {
+      success: false,
+      error_code: err.code || ERROR_SERVER_EXCEPTION,
+      error_message: 'Internal Server Error',
+    };
+  };
+  return callback();
+});
+
+
+/**
+ *
+ * @api {get} /search Search the address
+ * @apiName Search
+ * @apiVersion  1.0.0
+ * @apiHeader (AuthHeader) {String} Content-Type application/json
+ * @apiParam {string} address
+ * @apiParam {string="zh_hk","en_us"} lang
+ * @apiSuccessExample {type} Success-Response:
+ * {
+ *     success: true,
+ *     data: {
+ *        search_string: String,
+ *        results: [{
+ *            full_address: String,
+ *            coordinate: {
+ *              lat: Number,
+ *              lng: Number,
+ *            },
+ *            data_source: String,
+ *            elements: {
+ *                [key?]: String
+ *            }
+*         }]
+ *      }
+ * }
+ */
+server.get('/search', asyncMiddleware(searchAddress));
+
+server.listen(80, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
