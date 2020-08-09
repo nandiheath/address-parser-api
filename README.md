5d0fbec70aff66d16420e12bb8e2f233224d122f
Nandi Wong <nandi0315@gmail.com>
2020-08-09 12:25:23 +0800

    add docker file

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # address-parser-api
 
-This is a very simple project that act as a endpoint for using the [hk-address-parser-lib](https://github.com/g0vhk-io/hk-address-parser-lib)
+This is a simple project create endpoint for using the [hk-address-parser-lib](https://github.com/g0vhk-io/hk-address-parser-lib)
+
+## Run
+
+```bash
+# install dependencies
+yarn
+
+# run
+yarn start
+```
+
+will listen to port `3000` by default
+
+### EndPoints
+
+http://localhost:3000/search?address=[ADDRESS]&lang=[LANG]
+
+- `ADDRESS`: url encoded adress string
+- `LANG`: the output language. support `en_us` and `zh_hk`.
--- index.js
+++ index.js
@@ -2,47 +2,59 @@ const Promise = require('bluebird');
 const restify = require('restify');
 const request = Promise.promisifyAll(require('request'));
 const addressParser = require('hk-address-parser-lib');
+const { BadRequestError } = require('restify-errors');
 
 
 async function searchAddress(req, res, next) {
   const { address, lang } = req.query;
-  const addresses = await addressParser.parse(address);
 
-  const language = lang && lang.toLowerCase() === 'zh_hk' ? addressParser.Address.LANG_ZH : addressParser.Address.LANG_EN;
-  const results = addresses.map(address => {
-    const components = {};
-    const coordinate = address.coordinate();
-    address.components(language).forEach(function (component) {
-      components[component.key] = component.translatedValue;
+  if (!address) {
+    return next(new BadRequestError('address cannot be empty'));
+  }
+
+  try {
+    const addresses = await addressParser.parse(address);
+    const language = lang && lang.toLowerCase() === 'zh_hk' ? addressParser.Address.LANG_ZH : addressParser.Address.LANG_EN;
+    const results = addresses.map(address => {
+      const components = {};
+      const coordinate = address.coordinate();
+      address.components(language).forEach(function (component) {
+        components[component.key] = component.translatedValue;
+      });
+
+      return {
+        full_address: address.fullAddress(language),
+        coordinate: {
+          lat: parseFloat(coordinate.lat),
+          lng: parseFloat(coordinate.lng),
+        },
+        data_source: address.dataSource(),
+        elements: components,
+      }
     });
 
-    return {
-      full_address: address.fullAddress(language),
-      coordinate: {
-        lat: parseFloat(coordinate.lat),
-        lng: parseFloat(coordinate.lng),
-      },
-      data_source: address.dataSource(),
-      elements: components,
-    }
-  });
+
+    res.send({
+      success: true,
+      data: {
+        search_string: address,
+        results,
+      }
+    });
+    next();
+  } catch (error) {
+    return next(error.message);
+  }
 
 
-  res.send({
-    success: true,
-    data: {
-      search_string: address,
-      results,
-    }
-  });
-  next();
+
 }
 
 const asyncMiddleware = (fn) => {
   return (req, res, next) => {
     return fn(req, res, next).catch(error => {
       console.error(error);
-      next(error);
+      next('error');
     })
   }
 }
@@ -53,41 +65,30 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 // Catch the errors and format the response
-server.on('restifyError', (req, res, err, callback) => {
-  logger.debug(err.message);
-  logger.debug(err.stack);
+server.on('restifyError', function (req, res, err, callback) {
+  console.log(err);
   err.toJSON = function customToJSON() {
     return {
-      success: false,
-      error_code: err.code || ERROR_SERVER_EXCEPTION,
-      error_message: err.message,
+      name: err.name,
+      message: err.message
     };
   };
   err.toString = function customToString() {
-    return {
-      success: false,
-      error_code: err.code || ERROR_SERVER_EXCEPTION,
-      error_message: 'Internal Server Error',
-    };
+    return err.message || 'Internal server error';
   };
   return callback();
 });
-
 
 /**
  *
  * @api {get} /search Search the address
  * @apiName Search
  * @apiVersion  1.0.0
- * @apiHeader (Session) {String} auth-account-id Logged in User's Id
- * @apiHeader {String} authorization Logged in User's Id
- * @apiSuccess {String} data.search_string name of the user
- * @apiSuccess {String} data.results the result set
  * @apiParam {String="zh_hk", "en_us"} lang the language for the results
  * @apiParam {String} address the address to lookup
  */
 server.get('/search', asyncMiddleware(searchAddress));
 
-server.listen(80, function () {
+server.listen(process.env.PORT || 3000, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
